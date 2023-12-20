deb23f5621753eefc435bc71fb8beb479d4261de
Kortans <kortansyolo@gmail.com>
2023-12-20 09:15:16 +0100

    Ajout de fonction et modification d'existante pour prendre en charge les années. Crée : year(), updateYear(), isBissextile() ; Modifiée : Date(), isDate(), getDaysInMonth()

--- date.cpp
+++ date.cpp
@@ -13,9 +13,13 @@
 
 
 
-Date::Date(int month, int day) : _month(month), _day(day) {
-    bool status = isDate(month, day);
+Date::Date(int month, int day, int year) : _month(month), _day(day), _year(year) {
+    bool status = isDate(month, day, year);
     assert(status && "Date is not valid");
+}
+
+int Date::year() const{
+    return _year;
 }
 
 int Date::month() const {
@@ -26,14 +30,20 @@ int Date::day() const {
 	return _day;
 }
 
+void Date::updateYear(int year) {
+    bool status = isDate(_month, _day, year);
+    assert(status==true && "New year is not valid");
+    _year = year;
+}
+
 void Date::updateMonth(int month) {
-    bool status = isDate(month, _day);
+    bool status = isDate(month, _day, _year);
     assert(status==true && "New month is not valid");
     _month = month;
 }
 
 void Date::updateDay(int day) {
-    bool status = isDate(_month, day);
+    bool status = isDate(_month, day, _year);
     assert(status==true && "New day is not valid");
     _day = day;
 }
@@ -42,8 +52,9 @@ void Date::next() {
     if ((_month==12) && (_day==31)) {
         _day=1;
         _month=1;
+        _year++;
     }
-    else if (_day==getDaysInMonth(_month)) {
+    else if (_day==getDaysInMonth(_month, _year)) {
         _day=1;
         _month++;
     }
@@ -59,7 +70,7 @@ void Date::back() {
     }
     else if (_day==1) {
         _month--;
-        _day=getDaysInMonth(_month);
+        _day=getDaysInMonth(_month, _year);
     }
     else {
         _day--;
@@ -72,18 +83,26 @@ void Date::back() {
  * 
 */
 
-bool isDate(int month, int day) {
+bool isBissextile(int year){
+    if ((year%4 == 0 && year%100 != 0) || (year%400 == 0)) return true;
+    else return false;
+}
+
+
+bool isDate(int month, int day, int year=1) {
     if ((day < 1) || (day>31)) return false;
     if ((month <1) || (month>12)) return false;
-    if ((month == 2) && (day > 28)) return false;
+    if ((month == 2) && (day > 28) && (isBissextile(year) == false)) return false;
+    if ((month == 2) && (day > 29) && (isBissextile(year) == true)) return false;
     if (((month == 4) || (month == 6) || 
         (month == 9) || (month == 11)) && (day > 30)) return false;
     return true;
 }
 
-int getDaysInMonth(int month)  { 
+int getDaysInMonth(int month, int year)  { 
     assert(((month >=1) && (month<=12)) && "Month is not valid");
-    if (month == 2) return 28;
+    if ((month == 2) && (isBissextile(year) == false)) return 28;
+    if ((month == 2) && (isBissextile(year) == true)) return 29;
     if ((month == 1 || month == 3 || month == 5 || month == 7
     || month == 8 || month == 10 || month == 12)) return 31;
     return 30;
@@ -92,7 +111,7 @@ int getDaysInMonth(int month)  {
 int dayOfYear(Date d) {
     auto day=0;
     for (auto i=1;i<d.month();i++) {
-        day+=getDaysInMonth(i);
+        day+=getDaysInMonth(i, d.year());
     }
     day+= d.day();
     return day;
