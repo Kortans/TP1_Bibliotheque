96f60701e88e1869a1aa771145295ad61549a5c6
Kortans <kortansyolo@gmail.com>
2024-01-08 08:40:04 +0100

    Ajout de nouvelles commandes pour tester la classe Livre

--- main.cpp
+++ main.cpp
@@ -9,15 +9,25 @@
 
 
 #include "date.h"
+#include "livre.h"
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
-  Date a_day(7,31);
+  Date a_day(2,28,2004);
   std::cout << "a day: " << toString(a_day) << std::endl;
   std::cout << "#day: " << dayOfYear(a_day) << std::endl;
   a_day.next();
   std::cout << "a day + 1: " << toString(a_day) << std::endl;
   std::cout << "#day: " << dayOfYear(a_day) << std::endl;
 
+  Date sortiedubouquinla(04,05,1987);
+  Livre lebouquinla("The Hitchhiker's guide to the Galaxy","Douglas Adams","Science Fiction",sortiedubouquinla,"0593359445");
+  //std::cout << lebouquinla;
+  lebouquinla.ajoutEmprunteur("Test");
+  lebouquinla.ajoutEmprunteur("Test11");
+  std::cout << "Titre : "<<lebouquinla.titre()<<std::endl;
+  std::cout << "Dernier Emprunteur : "<<lebouquinla.dernierEmprunteur()<<std::endl;
+
+  std::cout << lebouquinla<<std::endl;
   return 0;
 }
